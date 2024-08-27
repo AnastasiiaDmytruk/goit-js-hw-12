d64d895a2a1e426921fa38dc130c67dc4b417135
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2024-08-27 13:37:55 +0200

    finish hw12

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import { getPictures } from './js/pixabay-api.js';
 import appendCards from './js/render-functions.js';
 
